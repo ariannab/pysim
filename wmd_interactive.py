be827bf919e34431eedfc49492888e52f111152e
Arianna Blasi <blasia@usi.ch>
2020-08-11 16:13:37 +0200

    Keep going with more examples if the user whishes so

--- wmd_interactive.py
+++ wmd_interactive.py
@@ -8,9 +8,14 @@ model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz
 # sentence_one = 'Obama speaks to media in Illinois'.lower().split()
 # sentence_two = 'The president greets press in Chicago'.lower().split()
 
-sentence_one = input("First sentence is... ")
-sentence_two = input("Second sentence is... ")
+choice = "y"
 
-# Compute WMD.
-distance = model.wmdistance(sentence_one, sentence_two)
-print(distance)
+while choice is not "n":
+	sentence_one = input("First sentence is... ")
+	sentence_two = input("Second sentence is... ")
+
+	# Compute WMD.
+	distance = model.wmdistance(sentence_one, sentence_two)
+	print("Distance is: ", distance)
+
+	choice = input("Press n to exit, or continue")
